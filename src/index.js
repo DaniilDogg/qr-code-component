ef815734c013a97b9c682b7fae1d991fd777df05
DaniilDogg <yurchenko.daniil.2001@gmail.com>
2023-02-22 23:17:18 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import App from './App';
 
+document.title = 'QR Code';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
